bafb4c482432ba944e96cf9718a739e6440aa88b
Elias Bistry <digitalis13@gmx.de>
2022-10-09 10:40:49 +0200

    add basic auth for neo4j

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,8 @@
 import * as neo4j from 'neo4j-driver'
 
 export const driver = neo4j.driver(
-    process.env.NEO4J_URI as string
+    process.env.NEO4J_URI as string,
+    neo4j.auth.basic(process.env.NEO4J_USER as string, process.env.NEO4J_PASSWORD as string)
 )
 
 export function transaction() {
